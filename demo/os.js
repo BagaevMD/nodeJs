1af08fd64fc1d653f990acaf5ed52148867155a9
BagaevMD <BagaeffMD@gmail.com>
2020-12-07 03:12:36 +0500

    remove some files

--- demo/os.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-
-console.log('System platform: ', os.platform());
-console.log('Processor: ', os.arch());
-console.log('Processor info: ', os.cpus());
-console.log('Free memory: ', os.freemem());
-console.log('Total memory ', os.totalmem());
-console.log('Home dir: ', os.homedir());
-console.log('System uptime ', os.uptime());
--- demo/path.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-console.log('Filename: ', path.basename(__filename));
-console.log('Dirname: ', path.dirname(__filename));
-console.log('Extname: ' ,path.extname(__filename));
-console.log('Parse: ', path.parse(__filename));
-console.log(path.join(__dirname, 'server', 'index.html'));
